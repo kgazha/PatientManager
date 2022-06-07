b95e4e2af15e11fcf67db30f17742159d8c41ca9
Konstantin Gazha <46375717+kgazha@users.noreply.github.com>
2022-06-07 22:28:20 +0500

    Add project files.

---
+++ PatientManager/PatientInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PatientManager
+{
+    internal class PatientInfo
+    {
+        public IEnumerable<dynamic> Receptions { get; set; }
+        public IEnumerable<dynamic> Patients { get; set; }
+
+        public PatientInfo()
+        {
+            var rand = new Random();
+
+            Receptions = Enumerable.Range(1, 500000).SelectMany(pid =>
+                Enumerable.Range(1, rand.Next(0, 100)).Select(rid => new
+                {
+                    PatientId = pid,
+                    ReceptionStart = new DateTime(2017, 06, 30).AddDays(-rand.Next(1, 500))
+                })).ToList();
+
+            Patients = Enumerable.Range(1, 500000)
+                .Select(pid => new
+                {
+                    Id = pid,
+                    Surname = string.Format("Иванов{0}", pid)
+                }).ToList();
+        }
+
+        public IEnumerable<dynamic> GetPatientsFirstCase(DateTime thresholdDateTime)
+            => Receptions.Join(Patients, r => r.PatientId, p => p.Id,
+                (r, p) => new { Surname = p.Surname, ReceptionStart = r.ReceptionStart })
+                .Where(i => i.ReceptionStart < thresholdDateTime).ToList();
+
+        public IEnumerable<dynamic> GetPatientsSecondCase(DateTime thresholdDateTime)
+        {
+            var filteredPatientIds = Receptions.Where(i => i.ReceptionStart < thresholdDateTime)
+                .Select(i => i.PatientId).ToHashSet();
+
+            return Patients.Where(p => filteredPatientIds.Contains(p.Id)).ToList();
+        }
+
+        public IEnumerable<dynamic> GetPatientsThirdCase(DateTime thresholdDateTime)
+        {
+            var filteredPatientIds = Receptions.Where(i => i.ReceptionStart < thresholdDateTime)
+                .Select(i => i.PatientId).ToList();
+
+            return Patients.Where(p => filteredPatientIds.Contains(p.Id)).ToList();
+        }
+    }
+}
---
+++ PatientManager/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PatientManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new PatientInfo();
+            var testDate = new DateTime(2017, 01, 01);
+
+            CalculateTheExecutionTime(() => manager.GetPatientsFirstCase(testDate));
+            CalculateTheExecutionTime(() => manager.GetPatientsSecondCase(testDate));
+            CalculateTheExecutionTime(() => manager.GetPatientsThirdCase(testDate));
+
+            Console.WriteLine();
+        }
+
+        public static void CalculateTheExecutionTime(Action action)
+        {
+            var tokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = tokenSource.Token;
+            cancellationToken.Register(() => action());
+            tokenSource.CancelAfter(10000);
+
+            var start = DateTime.Now;
+
+            var task = Task.Run(() => action.Invoke(), cancellationToken);
+
+            try
+            {
+                Task.WaitAny(new[] { task }, cancellationToken);
+                Console.WriteLine($"{action.Method.Name} : {DateTime.Now - start}");
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine($"{action.Method.Name} is too slow. Time has passed {DateTime.Now - start}");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("-------");
+            }
+        }
+    }
+}
